36184087d62755228010a36eeef216a87dd70551
mariana-martins <mariana.menezes@kathmandu.co.nz>
2020-04-23 19:44:21 +1200

    Code View Deck Screen

--- screens/ViewDeckScreen.js
+++ screens/ViewDeckScreen.js
@@ -1,9 +1,44 @@
-import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { getDeck } from '../utils/api';
 
 export default function ViewDeckScreen(props) {
-  const { navigation } = props;
-  return <View style={styles.container}></View>;
+  const { navigation, route } = props;
+  const { title } = route.params;
+
+  const [isLoading, setLoading] = useState(true);
+  const [deck, setDeck] = useState();
+
+  useEffect(() => {
+    getDeck(title)
+      .then((data) => setDeck(data))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{deck && deck.title ? deck.title : 'No name'}</Text>
+      <Text>
+        {deck && deck.questions ? `${deck.questions.length} cards` : 'No cards'}
+      </Text>
+      <Button
+        title="Add Card"
+        onPress={() => navigation.navigate('New Question', { title })}
+      />
+      <Button
+        title="Start Quiz"
+        onPress={() => navigation.navigate('Quiz', { title })}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
